095d4032a7aed7a4823d05524957b801893ec11d
PickingUsername <tangsihao100@qq.com>
2020-02-26 16:32:40 +0800

    构建项目，设置全局样式，elementui

---
+++ vue.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+module.exports = {
+  publicPath: './',
+  // 输出文件目录,默认就是dist
+  outputDir: process.env.NODE_ENV === "test" ? 'distTest' : 'distPro',
+  // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+  assetsDir: 'static',
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+  // 配置es-link true,'error'
+  lintOnSave: true,
+  css: {  // 一次配置，全局使用，这个scss 因为每个文件都要引入
+    loaderOptions: {
+      sass: {
+        prependData: `@import "./src/styles/main.scss";`
+      }
+    }
+  },
+  // webpack-dev-server 相关配置
+  devServer: {
+    // 可以通过设置让浏览器 overlay 同时显示警告和错误：
+    overlay: {
+      warnings: false,
+      errors: false
+    },
+    open: false,
+    host: 'localhost',
+    port: 8080,
+    https: false,
+    proxy: {
+      '/mgr': {
+        target: 'https://api.douban.com',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite:{
+          '^/mgr': ''
+        }
+      }
+    }
+  },
+  configureWebpack:{  // 覆盖webpack默认配置的都在这里
+    resolve:{   // 配置解析别名
+      alias:{
+        '@': path.resolve(__dirname, './src')
+      } 
+    }
+  }
+}
